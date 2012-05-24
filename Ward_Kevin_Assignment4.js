44fc71662fe406b6a7aa82bf2604ab306b858c2d
Kevin Ward <Xalicus@fullsail.edu>
2012-05-24 15:53:32 -0700

    I added Variables from older code.

--- Ward_Kevin_Assignment4.js
+++ Ward_Kevin_Assignment4.js
@@ -1,1 +1,19 @@
-alert("JavaScript works!");
+// Author: Kevin Ward
+// Date: May 24th, 2012
+// Name: Deliverable 4
+// Theme: Procrastination and Schoolwork.
+
+
+// Variables
+
+var playTime = true;
+var foodType = "hamburgers"; // "hamburgers" or "pasta"
+var bunToppings = ["mayo, mustard, BBQ sauce, and cheese slices" , "ketchup, mustard, relish, and cheese slices"];
+var pastaTypes = ["Chicken Alfredo" , "Spagetti"];
+var condosAvail = [ "sound" , "don\'t sound" ];
+var pastaAvail = [ "is" , "is" ];
+var myCash = 25; // It should be more than just $25. *Sniff, sniff*
+var storeNames = [ "GameStop", "Walmart", "Bookman\'s", "Movie Theater" ],
+	minPerStore = [ 50, 20, 30, 120 ];
+var hoursLeft = 2;
+
